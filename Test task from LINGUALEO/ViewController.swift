82afe571aafcd174eb96fe1cf95408692b380e9c
Tyshkan4ik <001vat@gmail.com>
2023-08-10 12:00:06 +0300

    start project

---
+++ Test task from LINGUALEO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test task from LINGUALEO
+//
+//  Created by Виталий Троицкий on 10.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
